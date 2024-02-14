e69db1bd9ebf9dbe1f19d077a7270ea20e81a2ee
SanzhIss <200103158@stu.sdu.edu.kz>
2024-02-14 12:52:49 +0600

    Last commit for 1st task

--- DiplomaProject/authentication/views.py
+++ DiplomaProject/authentication/views.py
@@ -1,9 +1,10 @@
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-
 
 # Create your views here.
 def home(request):
@@ -19,13 +20,37 @@ def signup(request):
         pass_reg = request.POST['pass_reg']
         pass_config = request.POST['pass_config']
 
+        if User.objects.filter(username=username):
+            messages.error(request, "Username already exist! Try other username")
+            return redirect('home')
+
+        # if User.objects.filter(email=email):
+        #     messages.error(request, "Email already registered")
+        #     return redirect('home')
+
+        if len(username) > 15:
+            messages.error(request, "Username must be under 15 characters")
+
+        if pass_reg != pass_config:
+            messages.error(request, "Password didn't match!")
+
+        if not username.isalnum():
+            messages.error(request, "Username must contain only Numbers and Letters!")
+            return redirect('home')
+
         theuser = User.objects.create_user(username, email, pass_reg)
         theuser.first_name = firstname
         theuser.last_name = lastname
 
         theuser.save()
 
-        messages.success(request, "Your Account has been successfully created!")
+        messages.success(request, "Your Account has been successfully created! We have sent you a confirmation via email. Please confirm it")
+
+        subject = "[Captivating Culinary GAME] Successful registration"
+        message = "Hello " + theuser.first_name + "!! \n" + "Welcome to \n Online platform designed as a captivating culinary game! \n We have sent you a confirmation email. please confirm your email in order to activate your account. \n\n Thank You for visiting our website! \n Admin"
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [theuser.email]
+        send_mail(subject, message, from_email, to_list, fail_silently =True)
 
         return redirect('signin')
 
---
+++ DiplomaProject/diplomaproject/info.py
@@ -0,0 +1,5 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = '200103158@stu.sdu.edu.kz'
+EMAIL_HOST_PASSWORD = 'san12345'
+EMAIL_PORT = 587
